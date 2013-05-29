85bf46114c33fb7ae31ba82a84777335e549908c
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-05-29 11:32:18 +0900

    Add stub_api_config_file! method for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,11 @@ begin
   Bundler.setup(:default, :development)
 rescue Bundler::BundlerError => e
   $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
+  $stderr.puts 'Run `bundle install` to install missing gems'
   exit e.status_code
 end
 
 require 'minitest/autorun'
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each {|f| require f}
 
+stub_api_config_file!
