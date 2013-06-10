65c008094998f2b60aaec1f16e96ec31a4328519
Takeyuki FUJIOKA <fujioka@rabbix.net>
2013-06-10 23:29:03 +0900

    requireが間違っているので修正

--- lib/sakura_cloud.rb
+++ lib/sakura_cloud.rb
@@ -8,4 +8,4 @@ load File.join(ENV['HOME'], ".sakura_cloud.rb")
 require_relative 'sakura_cloud/string_utils'
 require_relative 'sakura_cloud/request'
 require_relative 'sakura_cloud/response'
-require_relative 'sakura_cloud/abstract_api'
+require_relative 'sakura_cloud/abstract_api_class'
