4c29d77c8e02e243e2468ad132a426a5a6ec80b5
Takeyuki FUJIOKA <xibbar@gmail.com>
2013-06-16 01:29:39 +0900

    underscoreがおかしいので、修正 #12

--- lib/sakura_cloud/util.rb
+++ lib/sakura_cloud/util.rb
@@ -3,7 +3,11 @@ class String
     if self == 'ID'
       self.downcase
     else
-      self.split(/(?![a-z])(?=[A-Z])/).map{|s| s.downcase}.join('_') rescue self
+      self.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
     end
   end
 
@@ -11,7 +15,7 @@ class String
     if self == 'id'
       self.uppercase
     else
-      self.split('_').map{|s| s.capitalize}.join('')
+      self.split('/').map{|s| s.split('_').map{|t|t.capitalize}.join}.join('::')
     end
   end
 end
@@ -29,7 +33,7 @@ class Symbol
     if self == 'id'
       self.uppercase
     else
-      self.split('_').map{|s| s.capitalize}.join('')
+      self.split('/').map{|s| s.split('_').map{|t|t.capitalize}.join}.join('::')
     end
   end
 end
