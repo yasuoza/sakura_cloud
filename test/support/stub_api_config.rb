5f6faf8ab911cd93859e7a7e482f0bf0148999a8
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-06-16 23:34:38 +0900

    API_KEY, API_SECRETをSakuraCloud以下に設定

--- test/support/stub_api_config.rb
+++ test/support/stub_api_config.rb
@@ -6,12 +6,11 @@ def stub_api_config_file!
   ENV['HOME'] = tmp_dir.to_s
   File.open("#{tmp_dir}/.sakura_cloud.rb", 'w') do |config_file|
     config_file.write <<-RUBY
-      API_KEY    = 'aaaaaaaaaaaaaaaaaaaaaaaa'
-      API_SECRET = 'bbbbbbbbbbbbbbbbbbbbbbbb'
+      ::SakuraCloud::API_KEY    = 'aaaaaaaaaaaaaaaaaaaaaaaa'
+      ::SakuraCloud::API_SECRET = 'bbbbbbbbbbbbbbbbbbbbbbbb'
     RUBY
   end
   at_exit do
     FileUtils.remove_entry_secure(tmp_dir) if File.exist?(tmp_dir)
   end
 end
-
